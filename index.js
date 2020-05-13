96dda5642ba5d84da5edb013e8c613bcd10fcd82
Riley Stephens <rileste@cdw.com>
2020-05-13 16:05:15 -0500

    Commit toi trigger build

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved
 // Licensed under the MIT License.
 
 // index.js is used to setup and configure your bot
